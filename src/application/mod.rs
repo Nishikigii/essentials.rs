1516831dafb131373a82505514bd0677a742f2bf
andei_buite <andei.buite@outlook.com>
2024-06-26 19:32:25 +0800

    New application

---
+++ src/application/mod.rs
@@ -0,0 +1,52 @@
+use std::fmt::{Arguments, Error};
+
+#[cfg(test)]
+mod tests
+{
+    use super::*;
+
+    #[derive(Debug)]
+    struct App;
+
+    impl<'a> Application<'a> for App
+    {
+        const IDENTIFIER: &'a str = "com.example.application";
+    }
+
+    impl<'a> Binary<'a> for App 
+    {
+        type Settings = &'a str;
+        type Error = &'a str;
+    
+        fn start( settings: Self::Settings )-> Result<Box<Self>, Error> 
+        {
+            println!("app start with settings: {settings}");
+            Ok( Box::new( App {} ) )
+        }
+    }
+
+    #[test]
+    fn implement()
+    {
+        println!("{:?}", App::start("-test").unwrap())
+    }
+
+}
+
+/// application
+pub trait Application<'a>
+{
+    /// application identifier
+    const IDENTIFIER: &'a str;
+}
+
+/// executable application
+pub trait Binary<'a>: Application<'a> 
+{
+    type Settings;
+    type Error: Sized;
+
+    /// start the application with arguments
+    fn start( settings: Self::Settings )-> Result<Box<Self>, Error>;
+
+}
