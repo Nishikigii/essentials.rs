c47a24acd3fc709c7a60adf7f4670cb1ab0d6f0b
andei_buite <andei.buite@outlook.com>
2024-07-07 19:26:14 +0800

    Change application

--- src/application.rs
+++ src/application.rs
@@ -2,7 +2,7 @@
 pub trait Application<'a>
 {
     /// application identifier
-    const IDENTIFIER: &'a str;
+    fn identifier( &self )-> String;
 }
 
 /// executable application
@@ -12,38 +12,5 @@ pub trait Binary<'a>: Application<'a>
     type Error: Sized;
 
     /// start the application with arguments
-    fn start( settings: Self::Settings )-> Result<Box<Self>, Self::Error>;
-}
-
-#[cfg(test)]
-mod tests
-{
-    use super::*;
-
-    #[derive(Debug)]
-    struct App;
-
-    impl<'a> Application<'a> for App
-    {
-        const IDENTIFIER: &'a str = "com.example.application";
-    }
-
-    impl<'a> Binary<'a> for App 
-    {
-        type Settings = &'a str;
-        type Error = &'a str;
-    
-        fn start( settings: Self::Settings )-> Result<Box<Self>, Self::Error> 
-        {
-            println!("app start with settings: {settings}");
-            Ok( Box::new( App {} ) )
-        }
-    }
-
-    #[test]
-    fn implement()
-    {
-        println!("{:?}", App::start("-test").unwrap())
-    }
-
+    fn start( settings: Self::Settings )-> Result<Self,String> where Self: Sized;
 }
