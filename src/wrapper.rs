fb277e3759059c75374b1a8f5799b12d20081f4b
andei_buite <andei.buite@outlook.com>
2024-07-11 02:32:09 +0800

    New wrap

--- src/wrapper.rs
+++ src/wrapper.rs
@@ -2,3 +2,19 @@ pub mod lang;
 pub mod arch;
 pub mod reason;
 pub mod result;
+pub mod version;
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Wrap<I,U>
+{
+    pub instance: I,
+    pub unit: U
+}
+
+impl<I,U> Wrap<I,U>
+{
+    pub fn new(instance: I, unit: U) -> Self
+    {
+        Wrap { instance, unit }
+    }
+}
